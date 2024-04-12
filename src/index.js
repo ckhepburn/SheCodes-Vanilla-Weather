6dc91e1b80f232a9d638324880509bc004e29aba
ckhepburn <162160743+ckhepburn@users.noreply.github.com>
2024-04-12 13:22:12 -0400

    Attempting to add API

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
+function searchCity(city) {
+  let apiKey = '23ba5449atfff1c0e02c24fb555af64o';
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  console.log(apiUrl);
+}
+
 function handleSearchSubmit(event) {
   event.preventDefault();
   let serachInput = document.querySelector('#search-form-input');
   let cityElement = document.querySelector('#city');
   cityElement.innerHTML = serachInput.value;
+  searchCity(serachInput.value);
 }
 
 let searchFormElement = document.querySelector('#search-form');
