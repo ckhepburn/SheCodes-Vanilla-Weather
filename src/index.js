455b6f4580c3505ada78eb5874cd45f318d6b175
ckhepburn <162160743+ckhepburn@users.noreply.github.com>
2024-04-12 15:14:35 -0400

    Updated API to work and pull correct data

--- src/index.js
+++ src/index.js
@@ -1,20 +1,23 @@
 function refreshWeather(response) {
   let temperatureElement = document.querySelector('#temperature');
-  temperatureElement.innerHTML = response.data.temperature.current;
+  let temperature = response.data.temperature.current;
+  let cityElement = document.querySelector('#city');
+
+  cityElement.innerHTML = response.data.city;
+  temperatureElement.innerHTML = Math.round(temperature);
 }
 
 function searchCity(city) {
   let apiKey = '23ba5449atfff1c0e02c24fb555af64o';
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
-  axios.get(apiURL).then(refreshWeather);
+  axios.get(apiUrl).then(refreshWeather);
 }
 
 function handleSearchSubmit(event) {
   event.preventDefault();
-  let serachInput = document.querySelector('#search-form-input');
-  let cityElement = document.querySelector('#city');
-  cityElement.innerHTML = serachInput.value;
-  searchCity(serachInput.value);
+  let searchInput = document.querySelector('#search-form-input');
+
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector('#search-form');
